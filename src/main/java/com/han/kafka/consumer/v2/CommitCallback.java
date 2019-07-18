d6f37ca901e82b826558a498b74231b9f05dd437
hanlin01 <hanlin01@qianxin.com>
2019-07-18 10:54:00 +0800

    Kafka offset提交失败时候的一些安全措施

---
+++ src/main/java/com/han/kafka/consumer/v2/CommitCallback.java
@@ -0,0 +1,50 @@
+package com.han.kafka.consumer.v2;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Map;
+
+/**
+ * @author: Hanl
+ * @date :2019/7/18
+ * @desc:
+ */
+public interface CommitCallback {
+
+    /**
+     * 异步接口
+     *
+     * @param consumer
+     * @param offsets
+     */
+    default void onSuccess(ConsumerTask consumer, Map<TopicPartition, OffsetAndMetadata> offsets) {
+
+    }
+
+    /**
+     * 同步接口
+     *
+     * @param consumer
+     */
+    default public void onSuccess(ConsumerTask consumer) {
+
+    }
+
+
+    default public void onException(Throwable cause) {
+
+    }
+
+    /**
+     * 回调函数,来处理记录血缘关系,消息审计
+     * 这里需要对id处理持久化，异步批量去处理
+     * 对于处理时间不需要根据单条记录去做处理，依照kafka批次数据处理就好了
+     */
+    default void onLineage(ConsumerRecords<String, String> consumerRecord) {
+
+
+    }
+}
--- src/main/java/com/han/kafka/consumer/v2/DefaultCommitCallback.java
+++ src/main/java/com/han/kafka/consumer/v2/DefaultCommitCallback.java
@@ -1,8 +1,6 @@
-package com.han.kafka.consumer.support;
+package com.han.kafka.consumer.v2;
 
-import com.han.kafka.common.CommitCallback;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 
@@ -11,7 +9,7 @@ import java.util.Map;
 
 /**
  * @author: Hanl
- * @date :2019/5/30
+ * @date :2019/7/18
  * @desc:
  */
 @Slf4j
@@ -22,12 +20,12 @@ public class DefaultCommitCallback implements CommitCallback {
     private boolean failedCommitIf = false;
 
     @Override
-    public void onSuccess(final Consumer consumer, Map<TopicPartition, OffsetAndMetadata> offsets) {
+    public void onSuccess(final ConsumerTask consumerTask, Map<TopicPartition, OffsetAndMetadata> offsets) {
         //FIXME 比如在提交offset异常的时候该如何处理，保障系统的可靠性和一致性
         //程序执行到这里的时候表明，业务处理应该是处理成功的但提交offset失败，可能因为网络的原因或者其他的问题
         //可以尝试重试机制去保障、如果仍然不行？，我们可以在consume poll到records的时候将offset信息放在内存
         try {
-            consumer.commitSync(offsets, Duration.ofSeconds(timeout));
+            consumerTask.consumer.commitSync(offsets, Duration.ofSeconds(timeout));
         } catch (Exception e) {
             log.error("commit offset exception e={}", e);
         }
@@ -39,14 +37,17 @@ public class DefaultCommitCallback implements CommitCallback {
      * 客户端配置可以加入：retry.backoff.ms这个指定consumer在尝试重试提交时候可以延迟多少ms之后进行再次提交
      */
     @Override
-    public void onSuccess(final Consumer consumer) {
+    public void onSuccess(final ConsumerTask consumerTask) {
         try {
-            consumer.commitSync(Duration.ofSeconds(timeout));
+            consumerTask.consumer.commitSync(Duration.ofSeconds(timeout));
         } catch (Exception e) {
-            if (failedCommitIf) {
-
-            }
             log.error("commit offset exception e={}", e);
+            if (failedCommitIf) {
+                 //提交失败，可以采取异步的消息机制将该异常提交给消息通知系统
+
+                //end暂停消费线程
+                consumerTask.pauseConsumer();
+            }
         }
     }
 }
